5113264682df84f925b87dbc6d24cf70400cf4fe
vikram <vikram.nov.14@gmail.com>
2017-06-03 19:10:40 +0530

    support for batching

---
+++ DatasetBatcher.py
@@ -0,0 +1,147 @@
+import os.path
+import numpy as np
+from random import shuffle
+
+class DatasetBatcher:
+
+    dataset_map = None
+    training_images = []
+    testing_images = []
+    validation_images = []
+    training_labels_one_hot_vector = None
+    training_class_label = []
+    testing_labels_one_hot_vector = None
+    testing_class_label = []
+    validation_labels_one_hot_vector = None
+    validation_class_label = []
+    training_data_offset = 0
+    testing_data_offset = 0
+    validation_data_offset = 0
+
+    def __init__(self,dataset_map_,image_dir):
+        print "Initializing dataset batcher..."
+        self.dataset_map = dataset_map_
+        self._flatten_map(image_dir)
+
+
+    def _stack_vertically(self,target,source):
+        if target==None:
+            target = source
+        else:
+            target = np.vstack((target,source))
+        return target
+
+    def _flatten_map(self,image_dir):
+        num_classes = len(self.dataset_map.keys())
+        print "Number of classes:"+str(num_classes)
+        labels_array = []
+        for class_index, class_name in enumerate(self.dataset_map.keys()):
+            self.training_images.extend(self._create_fully_qualified_path_for_images(image_dir,self.dataset_map[class_name]['training'],class_name))
+            labels_array = self._create_one_hot_vector_matrix(len(self.dataset_map[class_name]['training']),class_index,num_classes)
+            self.training_labels_one_hot_vector = self._stack_vertically(self.training_labels_one_hot_vector,labels_array)
+            self.training_class_label.extend(self._duplicate_class_label(len(self.dataset_map[class_name]['training']),class_name))
+
+            self.testing_images.extend(self._create_fully_qualified_path_for_images(image_dir,self.dataset_map[class_name]['testing'],class_name))
+            labels_array = self._create_one_hot_vector_matrix(len(self.dataset_map[class_name]['testing']),class_index,num_classes)
+            self.testing_labels_one_hot_vector = self._stack_vertically(self.testing_labels_one_hot_vector,labels_array)
+            self.testing_class_label.extend(self._duplicate_class_label(len(self.dataset_map[class_name]['testing']),class_name))
+
+            self.validation_images.extend(self._create_fully_qualified_path_for_images(image_dir,self.dataset_map[class_name]['validation'],class_name))
+            labels_array = self._create_one_hot_vector_matrix(len(self.dataset_map[class_name]['validation']),class_index,num_classes)
+            self.validation_labels_one_hot_vector = self._stack_vertically(self.validation_labels_one_hot_vector,labels_array)
+            self.validation_class_label.extend(self._duplicate_class_label(len(self.dataset_map[class_name]['validation']),class_name))
+
+        self.training_images,self.training_labels_one_hot_vector,self.training_class_label = self._shuffle(self.training_images,self.training_labels_one_hot_vector,self.training_class_label)
+        self.testing_images,self.testing_labels_one_hot_vector,self.testing_class_label = self._shuffle(self.testing_images,self.testing_labels_one_hot_vector,self.testing_class_label)
+        self.validation_images,self.validation_labels_one_hot_vector,self.validation_class_label = self._shuffle(self.validation_images,self.validation_labels_one_hot_vector,self.validation_class_label)
+
+        print "Number of training labels:"+str(len(self.training_class_label))
+        print "Number of testing labels:"+str(len(self.testing_class_label))
+        print "Number of validation labels:"+str(len(self.validation_class_label))
+
+    """
+    Shuffles the dataset.
+    file_list: list of the file paths
+    file_label_one_hot_vectors: numpy array containing one hot encoded vectors
+    file_labels: list containing names of the classe of the images
+    """
+    def _shuffle(self,file_list,file_label_one_hot_vectors,file_labels):
+        file_list_shuff = []
+        file_label_one_hot_vectors_shuff = None
+        file_labels_shuff = []
+        index_shuf = range(len(file_list))
+        shuffle(index_shuf)
+        for i in index_shuf:
+            file_list_shuff.append(file_list[i])
+            file_labels_shuff.append(file_labels[i])
+            file_label_one_hot_vectors_shuff = self._stack_vertically(file_label_one_hot_vectors_shuff,file_label_one_hot_vectors[i,:])
+        return file_list_shuff,file_label_one_hot_vectors_shuff,file_labels_shuff
+
+    def _create_fully_qualified_path_for_images(self,image_dir,image_base_name_list,image_class_name):
+        return [os.path.join(image_dir, image_class_name, image_base_name) for image_base_name in image_base_name_list]
+
+    def _create_one_hot_vector_matrix(self,num_rows,class_index,num_classes):
+        one_hot_array = np.zeros((num_rows,num_classes), dtype=np.float32)
+        one_hot_array[:,class_index] = 1.0
+        return one_hot_array
+
+    def _duplicate_class_label(self,length_of_list,class_name):
+        dup_labels = []
+        for l in range(length_of_list):
+            dup_labels.append(class_name)
+        return dup_labels
+
+    """
+    Creates a new batch from the dataset
+    images_list: list containing all the images
+    image_labels_matrix: numpy matrix of one hot encoded vectors
+    image_labels: list containing names of the classe of the images
+    """
+    def _create_next_batch_(self,batch_size,offset,images_list,image_labels_matrix,image_labels):
+        if(offset>=len(images_list)):
+            return None,None,None
+        elif ((offset+batch_size)>len(images_list)):
+            min_index = offset
+            max_index = len(images_list)
+        else:
+            min_index = offset
+            max_index = offset+batch_size
+
+        image_paths = images_list[min_index:max_index]
+        labels_matrix = image_labels_matrix[min_index:max_index,:]
+        labels =image_labels[min_index:max_index]
+        return image_paths,labels_matrix,labels
+
+    def reset_training_offset(self):
+        self.training_data_offset = 0
+
+    def reset_testing_offset(self):
+        self.testing_data_offset = 0
+
+    def reset_validation_offset(self):
+        self.validation_data_offset = 0
+
+    def get_next_training_batch(self,batch_size):
+        return self._get_next_batch('training',batch_size)
+
+    def get_next_testing_batch(self,batch_size):
+        return self._get_next_batch('testing',batch_size)
+
+    def get_next_validation_batch(self,batch_size):
+        return self._get_next_batch('validation',batch_size)
+
+    """
+    category - training,testing,validation
+    batch_size - number of samples to return
+    """
+    def _get_next_batch(self,category,batch_size):
+        if category=='training':
+            image_paths,labels_matrix,labels = self._create_next_batch_(batch_size,self.training_data_offset,self.training_images,self.training_labels_one_hot_vector,self.training_class_label)
+            self.training_data_offset = self.training_data_offset + batch_size
+        elif category=='testing':
+            image_paths,labels_matrix,labels = self._create_next_batch_(batch_size,self.testing_data_offset,self.testing_images,self.testing_labels_one_hot_vector,self.testing_class_label)
+            self.testing_data_offset = self.testing_data_offset + batch_size
+        elif category=='validation':
+            image_paths,labels_matrix,labels = self._create_next_batch_(batch_size,self.validation_data_offset,self.validation_images,self.validation_labels_one_hot_vector,self.validation_class_label)
+            self.validation_data_offset = self.validation_data_offset+batch_size
+        return image_paths,labels_matrix,labels
---
+++ DatasetManager.py
@@ -0,0 +1,306 @@
+import tensorflow as tf
+import tarfile
+from osUtils import *
+import random
+from constants import *
+from six.moves import urllib
+import sys
+import numpy as np
+from PIL import Image
+
+def splitInTrainTestValSet(fileList,testPercentage,validationPercentage):
+  totalFiles = len(fileList)
+  random.shuffle(fileList)
+  trainPercentage = 100-testPercentage-validationPercentage
+  numTrainingFiles = int(totalFiles*trainPercentage/100)
+  numTestingFiles = int(totalFiles*testPercentage/100)
+  numValidationFiles = int(totalFiles*validationPercentage/100)
+
+  trainingSet = fileList[:numTrainingFiles]
+  testingSet = fileList[numTrainingFiles:numTrainingFiles+numTestingFiles]
+  validationSet = fileList[numTrainingFiles+numTestingFiles:]
+
+  return trainingSet,testingSet,validationSet
+
+def create_image_lists(image_dir, testingPercentage, validationPercentage):
+  if not tf.gfile.Exists(image_dir):
+    print("Image directory '" + image_dir + "' not found.")
+    return None
+
+  result = {}
+  # Get all the subdirectories. Each subdirectory would correspond to a class
+  subDirs = getSubdirectories(image_dir)
+  print "Processing Images from the subdirectories..."+str(subDirs)
+
+  # Valid image extensions
+  extensions = ['jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG']
+  fileListMap=getFilesFromDirectoriesWithExtension(subDirs,extensions)
+  for key,value in fileListMap.iteritems():
+	print "Number of Images in class:"+key+":"+str(len(value))
+	trainingSet,testingSet,validationSet = splitInTrainTestValSet(value,testingPercentage,validationPercentage)
+    	result[key] = {
+        	'dir': key,
+        	'training': trainingSet,
+        	'testing': testingSet,
+        	'validation': validationSet,
+    	}
+  return result
+
+def get_bottleneck_path_new(image_path, label_name, bottleneck_dir):
+	image_path = os.path.join(bottleneck_dir, label_name, os.path.basename(image_path))
+	return image_path + '.txt'
+
+def get_bottleneck_path(image_lists, label_name, index, bottleneck_dir,
+                        category):
+  """"Returns a path to a bottleneck file for a label at the given index.
+  Args:
+    image_lists: Dictionary of training images for each label.
+    label_name: Label string we want to get an image for.
+    index: Integer offset of the image we want. This will be moduloed by the
+    available number of images for the label, so it can be arbitrarily large.
+    bottleneck_dir: Folder string holding cached files of bottleneck values.
+    category: Name string of set to pull images from - training, testing, or
+    validation.
+  Returns:
+    File system path string to an image that meets the requested parameters.
+  """
+  return get_image_path(image_lists, label_name, index, bottleneck_dir,
+                        category) + '.txt'
+
+def get_image_path(image_lists, label_name, index, image_dir, category):
+  """"Returns a path to an image for a label at the given index.
+  Args:
+    image_lists: Dictionary of training images for each label.
+    label_name: Label string we want to get an image for.
+    index: Int offset of the image we want. This will be moduloed by the
+    available number of images for the label, so it can be arbitrarily large.
+    image_dir: Root folder string of the subfolders containing the training
+    images.
+    category: Name string of set to pull images from - training, testing, or
+    validation.
+  Returns:
+    File system path string to an image that meets the requested parameters.
+  """
+  if label_name not in image_lists:
+    tf.logging.fatal('Label does not exist %s.', label_name)
+  label_lists = image_lists[label_name]
+  if category not in label_lists:
+    tf.logging.fatal('Category does not exist %s.', category)
+  category_list = label_lists[category]
+  if not category_list:
+    tf.logging.fatal('Label %s has no images in the category %s.',
+                     label_name, category)
+  mod_index = index % len(category_list)
+  base_name = category_list[mod_index]
+  sub_dir = label_lists['dir']
+  full_path = os.path.join(image_dir, sub_dir, base_name)
+  return full_path
+
+def create_bottleneck_file_new(bottleneck_path, image_path, label_name,sess, inceptionV3Model):
+  """Create a single bottleneck file."""
+  print('Creating bottleneck at ' + bottleneck_path)
+
+  if not tf.gfile.Exists(image_path):
+    tf.logging.fatal('File does not exist %s', image_path)
+  #image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+  image = Image.open(image_path)
+  image_data = image.convert('RGB')
+  try:
+    bottleneck_values = inceptionV3Model.run_bottleneck_on_image(
+        sess, image_data)
+  except Exception as e:
+	print e
+	raise RuntimeError('Error during processing file %s' % image_path)
+
+  bottleneck_string = ','.join(str(x) for x in bottleneck_values)
+  with open(bottleneck_path, 'w') as bottleneck_file:
+    bottleneck_file.write(bottleneck_string)
+
+def create_bottleneck_file(bottleneck_path, image_lists, label_name, index,
+                           image_dir, category, sess, inceptionV3Model):
+  """Create a single bottleneck file."""
+  print('Creating bottleneck at ' + bottleneck_path)
+  image_path = get_image_path(image_lists, label_name, index,
+                              image_dir, category)
+  if not tf.gfile.Exists(image_path):
+    tf.logging.fatal('File does not exist %s', image_path)
+  #image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+  image = Image.open(image_path)
+  image_data = image.convert('RGB')
+  try:
+    bottleneck_values = inceptionV3Model.run_bottleneck_on_image(
+        sess, image_data)
+  except Exception as e:
+	print e
+	raise RuntimeError('Error during processing file %s' % image_path)
+
+  bottleneck_string = ','.join(str(x) for x in bottleneck_values)
+  with open(bottleneck_path, 'w') as bottleneck_file:
+    bottleneck_file.write(bottleneck_string)
+
+def get_or_create_bottleneck_new(sess, image_path,image_label,bottleneck_dir,inceptionV3Model):
+  """Retrieves or calculates bottleneck values for an image.
+  If a cached version of the bottleneck data exists on-disk, return that,
+  otherwise calculate the data and save it to disk for future use.
+  Args:
+    sess: The current active TensorFlow Session.
+    image_lists:image path.
+    bottleneck_dir: Folder string holding cached files of bottleneck values.
+  Returns:
+    Numpy array of values produced bget_bottleneck_pathy the bottleneck layer for the image.
+  """
+  sub_dir_path = os.path.join(bottleneck_dir, image_label)
+  ensure_dir_exists(sub_dir_path)
+  bottleneck_path = get_bottleneck_path_new(image_path, image_label, bottleneck_dir)
+
+  if not os.path.exists(bottleneck_path):
+    create_bottleneck_file_new(bottleneck_path, image_path, image_label, sess, inceptionV3Model)
+  with open(bottleneck_path, 'r') as bottleneck_file:
+    bottleneck_string = bottleneck_file.read()
+  did_hit_error = False
+  try:
+    bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+  except ValueError:
+    print('Invalid float found, recreating bottleneck')
+    did_hit_error = True
+  if did_hit_error:
+    create_bottleneck_file_new(bottleneck_path, image_path, image_label, sess, inceptionV3Model)
+    with open(bottleneck_path, 'r') as bottleneck_file:
+      bottleneck_string = bottleneck_file.read()
+    # Allow exceptions to propagate here, since they shouldn't happen after a
+    # fresh creation
+    bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+  return bottleneck_values
+
+
+def get_or_create_bottleneck(sess, image_lists, label_name, index, image_dir,
+                             category, bottleneck_dir,inceptionV3Model):
+  """Retrieves or calculates bottleneck values for an image.
+  If a cached version of the bottleneck data exists on-disk, return that,
+  otherwise calculate the data and save it to disk for future use.
+  Args:
+    sess: The current active TensorFlow Session.
+    image_lists: Dictionary of training images for each label.
+    label_name: Label string we want to get an image for.
+    index: Integer offset of the image we want. This will be modulo-ed by the
+    available number of images for the label, so it can be arbitrarily large.
+    image_dir: Root folder string  of the subfolders containing the training
+    images.
+    category: Name string of which  set to pull images from - training, testing,
+    or validation.
+    bottleneck_dir: Folder string holding cached files of bottleneck values.
+    jpeg_data_tensor: The tensor to feed loaded jpeg data into.
+    bottleneck_tensor: The output tensor for the bottleneck values.
+  Returns:
+    Numpy array of values produced bget_bottleneck_pathy the bottleneck layer for the image.
+  """
+  label_lists = image_lists[label_name]
+  sub_dir = label_lists['dir']
+  sub_dir_path = os.path.join(bottleneck_dir, sub_dir)
+  ensure_dir_exists(sub_dir_path)
+  bottleneck_path = get_bottleneck_path(image_lists, label_name, index,
+                                        bottleneck_dir, category)
+  if not os.path.exists(bottleneck_path):
+    create_bottleneck_file(bottleneck_path, image_lists, label_name, index,
+                           image_dir, category, sess, inceptionV3Model)
+  with open(bottleneck_path, 'r') as bottleneck_file:
+    bottleneck_string = bottleneck_file.read()
+  did_hit_error = False
+  try:
+    bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+  except ValueError:
+    print('Invalid float found, recreating bottleneck')
+    did_hit_error = True
+  if did_hit_error:
+    create_bottleneck_file(bottleneck_path, image_lists, label_name, index,
+                           image_dir, category, sess, inceptionV3Model)
+    with open(bottleneck_path, 'r') as bottleneck_file:
+      bottleneck_string = bottleneck_file.read()
+    # Allow exceptions to propagate here, since they shouldn't happen after a
+    # fresh creation
+    bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+  return bottleneck_values
+
+def get_random_cached_bottlenecks_new(sess, image_paths, image_labels, bottleneck_dir, inceptionV3Model):
+  """Retrieves bottleneck values for cached images.
+  If no distortions are being applied, this function can retrieve the cached
+  bottleneck values directly from disk for images. It picks a random set of
+  images from the specified category.
+  Args:
+    sess: Current TensorFlow Session.
+    image_paths: List of image paths.
+	image_labels: List of the image labels
+    bottleneck_dir: Folder string holding cached files of bottleneck values.
+  Returns:
+    List of bottleneck arrays.
+  """
+  bottlenecks = []
+  for i in range(len(image_paths)):
+	  bottleneck = get_or_create_bottleneck_new(sess, image_paths[i], image_labels[i], bottleneck_dir, inceptionV3Model)
+	  bottlenecks.append(bottleneck)
+  return bottlenecks
+
+
+def get_random_cached_bottlenecks(sess, image_lists, how_many, category,
+                                  bottleneck_dir, image_dir, inceptionV3Model):
+  """Retrieves bottleneck values for cached images.
+  If no distortions are being applied, this function can retrieve the cached
+  bottleneck values directly from disk for images. It picks a random set of
+  images from the specified category.
+  Args:
+    sess: Current TensorFlow Session.
+    image_lists: Dictionary of training images for each label.
+    how_many: If positive, a random sample of this size will be chosen.
+    If negative, all bottlenecks will be retrieved.
+    category: Name string of which set to pull from - training, testing, or
+    validation.
+    bottleneck_dir: Folder string holding cached files of bottleneck values.
+    image_dir: Root folder string of the subfolders containing the training
+    images.
+    jpeg_data_tensor: The layer to feed jpeg image data into.
+    bottleneck_tensor: The bottleneck output layer of the CNN graph.
+  Returns:
+    List of bottleneck arrays, their corresponding ground truths, and the
+    relevant filenames.
+  """
+  class_count = len(image_lists.keys())
+  bottlenecks = []
+  ground_truths = []
+  filenames = []
+  if how_many >= 0:
+    # Retrieve a random sample of bottlenecks.
+    for unused_i in range(how_many):
+      label_index = random.randrange(class_count)
+      label_name = list(image_lists.keys())[label_index]
+      image_index = random.randrange(MAX_NUM_IMAGES_PER_CLASS + 1)
+      image_name = get_image_path(image_lists, label_name, image_index,
+                                  image_dir, category)
+      bottleneck = get_or_create_bottleneck(sess, image_lists, label_name,
+                                            image_index, image_dir, category,
+                                            bottleneck_dir, inceptionV3Model)
+      ground_truth = np.zeros(class_count, dtype=np.float32)
+      ground_truth[label_index] = 1.0
+      bottlenecks.append(bottleneck)
+      ground_truths.append(ground_truth)
+      filenames.append(image_name)
+  else:
+    # Retrieve all bottlenecks.
+    for label_index, label_name in enumerate(image_lists.keys()):
+      for image_index, image_name in enumerate(
+          image_lists[label_name][category]):
+        image_name = get_image_path(image_lists, label_name, image_index,
+                                    image_dir, category)
+        bottleneck = get_or_create_bottleneck(sess, image_lists, label_name,
+                                              image_index, image_dir, category,
+                                              bottleneck_dir,inceptionV3Model)
+        ground_truth = np.zeros(class_count, dtype=np.float32)
+        ground_truth[label_index] = 1.0
+        bottlenecks.append(bottleneck)
+        ground_truths.append(ground_truth)
+        filenames.append(image_name)
+  return bottlenecks, ground_truths, filenames
+
+def readDataset(FLAGS):
+	# Look at the folder structure, and create lists of all the images.
+  	imageMap = create_image_lists(FLAGS.image_dir, FLAGS.testing_percentage,FLAGS.validation_percentage)
+	return imageMap
